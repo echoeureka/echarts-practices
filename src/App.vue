250c46be579235bab4781aa9eddb4e44e8f76124
cabbage9 <iherewithmyheart@gmail.com>
2021-07-09 19:20:46 +0800

    🔥 remove unnecessary code

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template></template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import echarts from 'echarts'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
+
+<style lang="scss" scoped>
+#app {
+  width: 80%;
+}
+</style>
